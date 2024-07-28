49a1211e9e6624fa6f2ef93f3b10e09391bc9c20
itzvishakha202 <sonawanevishakha94@gmail.com>
2024-07-28 22:15:48 +0530

    sum of odd numbers

---
+++ index.js
@@ -0,0 +1,23 @@
+// sum of odd number of given array
+const numbers = [2, 3, 10, 13, 9, 12, 8];
+let sumOdd = 0;
+
+for(let i=0; i < numbers.length; i++)
+{
+if(numbers[i] % 2 == 1){
+   sumOdd += numbers[i];
+}
+}
+console.log(`sum of odd numbers: ${sumOdd}`);
+
+// say hello to all elements in array
+// const employees = ["priya",  "omkar", "jay",  "abhi",  "ram",  "sita"]
+
+// for (let i=0; i < employees.length; i++)
+//     {
+//     console.log(`Hello ${employees[i]}`);
+// }
+
+// //finding largest number of the array
+// const number = [2, 4, 5, 8, 8, 10, 12, 16];
+
